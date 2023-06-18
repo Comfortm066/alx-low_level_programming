b8527ae213eb43a8ed5e7974b13b46abec415160
Comfortm066 <comfortm066@gmail.com>
2023-06-18 10:40:30 -0700

    4*

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z'){
+		if (letter != 'e' && letter != 'q')
+	{
+		putchar(letter);
+		letter++;
+	}}
+	return (0);
+}
