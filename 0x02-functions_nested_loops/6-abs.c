5af5b9b0847b330548e6bdce71cb8fcba3cb63aa
Comfortm066 <comfortm066@gmail.com>
2023-06-21 03:49:18 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: The absolute value of a number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (n * -1);
+	}
+	else
+	{
+	return (n);
+	}
+}
