7ca3de0c78630b81c3ccdd15e41bc4f479830031
Comfortm066 <comfortm066@gmail.com>
2023-06-21 05:02:08 -0700

    11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_to_98 - Adds two integers.
+ * @n: The number to start printing from
+ *
+ * Return: The result of the addition.
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+	if (n >= 98)
+	{
+	}
+	}
+
+}
+
