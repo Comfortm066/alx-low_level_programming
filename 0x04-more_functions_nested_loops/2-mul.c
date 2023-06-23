07547937427663ad1dfeb31f1f01a6f170a3d7c1
Comfortm066 <comfortm066@gmail.com>
2023-06-23 12:47:24 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+return (c);
+}
