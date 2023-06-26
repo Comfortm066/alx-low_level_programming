c9d0d8595d810511129d42d373277e1da15abd4c
Comfortm066 <comfortm066@gmail.com>
2023-06-26 06:46:38 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strlen.c -o 2-strlen

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,6 @@
-#include "main.h"
-/**
-* swap_int - swaps the value of int a and int b
-* @a: first int
-* @b: second int
-* Return 0
-*/
-void swap_int(int *a, int *b)
-{
-		int s;
-
-		s = *a;
-		*a = *b;
-		*b = s;
-}
+[master 6a82abb] 1-swap.c
+ 4 files changed, 43 insertions(+), 11 deletions(-)
+ rewrite 0x05-pointers_arrays_strings/0-reset_to_98.c (99%)
+ create mode 100644 0x05-pointers_arrays_strings/1-main.c
+ create mode 100755 0x05-pointers_arrays_strings/1-swap
+ create mode 100644 0x05-pointers_arrays_strings/1-swap.c
