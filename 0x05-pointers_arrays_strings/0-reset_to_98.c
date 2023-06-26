6a82abb32c0ce1385661900ac88aa83f470c3125
Comfortm066 <comfortm066@gmail.com>
2023-06-26 06:36:04 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,8 @@
-#include "main.h"
-/**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Return: 0 is success
-*/
-
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
+[master 7c7172b] gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
+ 6 files changed, 61 insertions(+)
+ create mode 100755 0x05-pointers_arrays_strings/0-98
+ create mode 100644 0x05-pointers_arrays_strings/0-main.c
+ create mode 100644 0x05-pointers_arrays_strings/0-reset_to_98.c
+ create mode 100644 0x05-pointers_arrays_strings/README.md
+ create mode 100644 0x05-pointers_arrays_strings/_putchar
+ create mode 100644 0x05-pointers_arrays_strings/main.h
